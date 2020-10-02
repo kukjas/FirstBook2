29338444f86fadf1b5de5a59702ad953ba9ac489
kukja <kukja@DESKTOP-OV2ABN2>
2020-10-02 16:58:48 +0900

    7.4.2 학습완료

---
+++ src/sec07/_04_01/Calculator.java
@@ -0,0 +1,8 @@
+package sec07._04_01;
+
+public class Calculator {
+		double areaCircle(double r) {
+			System.out.println("Calculator °´Ã¼ÀÇ areaCircle() ½ÇÇà");
+			return 3.14159 * r * r;
+		}
+}
